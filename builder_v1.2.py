e281c8a9f8760cb3d1b631edf5d3a88aa68f64db
Diaconescu Maria Raluca <44669618+Sofia190@users.noreply.github.com>
2021-07-12 15:55:26 +0200

    Update builder_v1.2.py

--- builder_v1.2.py
+++ builder_v1.2.py
@@ -312,6 +312,9 @@ class AstroNet():
 
 			def add_friends_from_the_same_network(self, name, date_of_birth, country, friends, friends_network_message="Friends network message"):
 
+					#class which calls another class which calls another class
+
+					#using specific attributes from the initial class
 
 					class Friends_network(AstroNet.Sphere.Friend):
 
