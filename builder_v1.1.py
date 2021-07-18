ea9e66865a8d5ca658c0331e4a86c71fa486d09f
Diaconescu Maria Raluca <44669618+Sofia190@users.noreply.github.com>
2021-07-18 08:32:59 +0200

    Update builder_v1.1.py

--- builder_v1.1.py
+++ builder_v1.1.py
@@ -19,7 +19,7 @@ from datetime import date as date_module
 
 
 
-print("Welcome to astro net!")
+print("Welcome to AstroNet!")
 print("                                     *                                            ")
 print("                                    * *                                           ")
 print("                                   *   *                                          ")
