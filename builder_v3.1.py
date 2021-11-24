d9a4f6b54d7fae4ca366eee9defae39d97830d23
Maria Raluca Diaconescu <44669618+MRDiaconescu@users.noreply.github.com>
2021-11-24 17:04:50 +0100

    Update builder_v3.1.py

--- builder_v3.1.py
+++ builder_v3.1.py
@@ -2639,6 +2639,36 @@ class AstroNet():
 				print(self.B_second_level_connections_list_3)
 
 				return self
+			
+			
+			def create_instances_for_three_levels(self):
+
+				child = self.create_instances_for_two_levels()
+				polyhedron3 = AstroNet().Sphere().Polyhedron().create_the_polyhedron()
+				polyhedron_3rd_level = AstroNet().Sphere().Polyhedron().create_the_polyhedron()
+
+				polyhedron3_connections2 = polyhedron3.third_level_connections()
+				polyhedron_3rd_level_connections1 = polyhedron_3rd_level.first_level_connections()
+
+				polyhedron3_connection1_points = [item for item in polyhedron3_connections2 if ((item[1] != "Y") and (item[1] != "Y'"))]
+				polyhedron_3rd_level_connection1_points = [item for item in polyhedron_3rd_level_connections1]
+				
+				# 2nd dimension
+
+				for item, item1 in zip(polyhedron3_connection1_points, polyhedron_3rd_level_connection1_points):
+					self.B_third_level_connections_2.append([item, item1])
+				print(self.B_third_level_connections_2)
+				
+				#3rd dimension
+
+				B_third_level_connections_2_1 = [item for item in self.B_third_level_connections_2]
+				B_third_level_connections_2_2 = [item[0] for item in B_third_level_connections_2_1]
+				B_third_level_connections_2_3 = [item[1] for item in B_third_level_connections_2_1]
+
+				for item, item1 in zip(B_third_level_connections_2_2, B_third_level_connections_2_3):
+					self.B_third_level_connections_3.append([item[0], item1[0]])
+				print(self.B_third_level_connections_3)
+
 
 
 		
